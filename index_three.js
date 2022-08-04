68383c82b5b2b09a6baea0d42b9e911935f4cecc
Samuel Njenga <snjenga704@gmail.com>
2022-08-04 22:50:04 +0300

    feat: add index_three.js

---
+++ index_three.js
@@ -0,0 +1,27 @@
+// YOU DON'T NEED COMMENTS IF YOU WRITE CLEAN CODE
+// HOWEVER THE ABOVE STATENT IS A PULLSH
+// SOMETIMES YOU WRITE COMMENTS BECAUSE CODE CAN DESCRIBE HOW, BUT IT CAN'T EXPLAIN WHY
+// USES OF COMMENTS
+// (i) What is the why
+// (ii) Clarify what is not typically legible by humans
+// WHO IS SMART? WE ARE!
+// WHO WRITES CLEAN CODE? WE DO!
+// LEVEL UP IN DIFFERENT WAYS BECAUSE THE CODE WHICH I THOUGHT WAS CLEAN 5 YEARS AGO IS NO LONGER THAT WAY
+// SOMETIMES WE ARE ALL KNOWLEDGEABLE IN DIFFERENT WAYS
+// IF YOU ARE IN A TEAM WHERE EVERYBODY'S SKILL SET IS MORE TIGHTLY KNIT YOU CAN BE IN THE SAME PAGE
+// IN SOME TEAMS, THE SKILL SETS ARE MORE OF A VENN DIAGRAM, YOU MAY NOT HAVE THE SAME OPINIONS OR BACKGROUNDS. IN MOST CASES, SUCH TEAMS HAVE A GREATER GROUND
+// (iii) Comments can be like chapters of a book -> So that you can scheme easier and pick things up faster
+// Sections of code allows people to get to the piece that is most relevant to them
+// (iv) A guide to keep the logic straight while writing the code so that you can break down your tasks into smaller bits 
+// Remove them later
+// FOR EXAMPLE;
+// get the request from the server
+// give us an error if it failed
+// do x thing with that request
+// format the data like so
+// (v) This is okay to refactor
+// this isn't my best work 
+// but we had to get it in by the deadline
+// (vi) Commenting as a teaching tool -> Keep yourself accountable for what you are learning
+// (vii) Stack Overflowed code
+// Senior Engineers should help team be as productive as possible and improve the scalablity of code
