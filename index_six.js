22be883b6f9d6ce3ffb1b23e4d1fd15bfebec43b
Samuel Njenga <snjenga704@gmail.com>
2022-08-05 13:23:49 +0300

    feat: add index_six.js

---
+++ index_six.js
@@ -0,0 +1,41 @@
+// What makes a GOOD comment?
+// (i) Short and concise -> Easy to maintain. In most cases it should not exceed one line.
+
+// (ii) Provides value.
+// Informative Comment
+// format dd/mm/yy or dd-mm-yyyy or dd.mm.yyyy
+
+// (iii) Clarifies code that has a business purpose but looks out of sort. For example there might be some domain knowledge in the system that some new developers may not be familiar with
+
+// (iv) Sometimes a warning or amplification of importance -> Do this to protect ourselves or provide the function that's needed
+// For example:
+// outstanding issue when UI layer is injecting empty characters. DO NOT remove TRIM()
+
+// (v) Legal Comments -> Used when working with Open Source projects
+// For example:
+/**
+ * This uses ROS Library 1.x.x and my organization assumes all risks in doing so.
+ * This comment is mandated by the license to use ROS Library 2022.
+ */
+
+/* SUMMARY */
+// Good code is Self-Documenting and Professional Programming Means Commenting
+
+// KEY REASONS FOR WRITING COMMENTS IN YOUR CODE:
+// -COMMUNICATION
+// -CLARITY
+// -EXPLANATION
+
+// TYPES OF CODE COMMENTS
+// -DOCUMENTATION COMMENTS -> Quickly clarifies the purpose why this code.
+// -FUNCTION COMMENTS -> It explains the function well.
+// -LOGIC COMMENTS -> It explains the logic of clarifying complex code paths.
+
+// TIPS FOR QUALITY PROFESSIONAL COMMENTS:
+// (i) Write comments while writing code so that you will remember.
+// (ii) Write short, informative comments, since if you can't write a clear comment, there may be a problem with the code.
+// Your comment should not create confusion, it should disappear confusion.
+// (iii) Write down why you are doing something.
+// (iv) Add comments while fixing bugs because sometimes fixing an issue may generate other issues as well
+// (v) Don't refer to other documents or comments
+// A useless comment or a comment that does not add or generate any value should be ommited. Ignore this type of comment.
